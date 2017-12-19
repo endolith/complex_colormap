f5e71b9d1c7ec2d5c36872f9f2e14d28316c04de
Nico Schlömer <nico.schloemer@gmail.com>
2017-12-19 20:45:09 +0100

    bring the repo into python-package form

---
+++ complex_colormap/__about__.py
@@ -0,0 +1,8 @@
+# -*- coding: utf-8 -*-
+#
+__version__ = '0.0.1'
+__author__ = 'Phil Elson'
+__author_email__ = 'endolith@gmail.com'
+__website__ = 'https://github.com/endolith/complex_colormap'
+__license__ = 'License :: OSI Approved :: MIT License'
+__status__ = 'Development Status :: 2 - Pre-Alpha'
---
+++ complex_colormap/__init__.py
@@ -0,0 +1,11 @@
+# -*- coding: utf-8 -*-
+#
+from __future__ import print_function
+
+from .__about__ import (
+        __version__,
+        __website__,
+        )
+
+from . import cplot
+from . import generation
