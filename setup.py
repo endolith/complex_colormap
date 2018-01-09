6f25d1fce2e96026876fcd173bfe290bd86752f5
endolith <endolith@gmail.com>
2018-01-08 22:52:27 -0500

    Fix project name in setup.py

--- setup.py
+++ setup.py
@@ -7,7 +7,8 @@ import codecs
 # https://packaging.python.org/single_source_version/
 base_dir = os.path.abspath(os.path.dirname(__file__))
 about = {}
-with open(os.path.join(base_dir, 'pygmsh', '__about__.py'), 'rb') as f:
+with open(os.path.join(base_dir, 'complex_colormap', '__about__.py'),
+          'rb') as f:
     exec(f.read(), about)
 
 
